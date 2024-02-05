386bb212e47392151ba862eab6feb63fdb9450a8
Sedat Yilmaz <sedatyilmaz.co@gmail.com>
2024-02-05 11:30:03 +0300

    .

--- Models/BaseModel.cs
+++ Models/BaseModel.cs
@@ -8,12 +8,7 @@ namespace Class_Model_Validation.Models
 {
     public class ReturnModel
     {
-        internal bool _result { get; set; }
-        internal long? _errorcode { get; set; }
-        internal object _data { get; set; }
-        internal string _errormessage { get; set; }
-        internal Exception _ex { get; set; }
-
+    
         public bool Result { get; set; }
         public object Data { get; set; }
         public Exception Exception { get; set; }
